f627f2e1b37fa6953d6dfa55634645d8c63f371f
Shane <shanematthiasloong@hotmail.com>
2021-11-25 16:22:25 -0500

    Updated notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,10 @@
 /*
+React is virtual DOM
+DOM is tree-structured
+Reconciliation: Syncing DOM to Virtual Dom
+Diffing: defrentiating copy previous DOM to update DOM
+Declaring keys on elements to increase performance when adding to top of list
+
 var is function scope
 let is block scope
 
@@ -14,7 +20,34 @@ type of null = object
 type of undefined = undefined
 both are empty values
 
-function expressoin let func = function()
-function decleration = function func() 
+function decleration = function func() cannot be passed into another function
+function expression let func = function() cannot be called before its declared
+behaves like a variable
 
+normal functions: "this" depends on function invocation 
+arrow functions: "this" is always referring to the outer function
+
+simple invocation, method invocation, 
+indirect invocation, constructor invocation
+
+arguments in arrow function is taken from parent function
+accessing direct arguments of the arrow function requires
+the rest parameters as the function paramter "...args"
+https://dmitripavlutin.com/differences-between-arrow-and-regular-functions/
+
+prototype inheritance: creating multiple instances of a class.
+Accessing the prototype method by calling "name.method()"
+
+Babel tool converst JSX to HTML
+Babel tool make sure support for ES5 - ES2019 on browsers
+
+<Parent>
+    <Child />
+</Parent>
+{props.children} in Child component
+
+You can create <Segmant> components that returns <div> with specificd style
+
+Destructuring variables
+const [ el1, el2 ] = arr
 */
